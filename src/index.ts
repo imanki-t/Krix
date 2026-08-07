aeed702b42d1e42452fb3357b2bdf82341871883
Lyc <106875305+imanki-t@users.noreply.github.com>
2026-08-08 00:50:59 +0530

    Import makeRegistrar in src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ import { registerGitHubTools } from './githubTools.js';
 import { registerRenderTools } from './renderTools.js';
 import { registerSandboxTools, destroySandbox } from './sandboxTools.js';
 import {
-  formatOptimizedResponse, getToolAnnotations, TOOL_CATEGORY, ToolCategory, getSessionContext, registerSessionAuth
+  formatOptimizedResponse, getToolAnnotations, TOOL_CATEGORY, ToolCategory, getSessionContext, registerSessionAuth, makeRegistrar
 } from './security.js';
 
 dotenv.config();
